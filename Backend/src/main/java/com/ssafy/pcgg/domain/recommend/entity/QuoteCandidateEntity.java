741380f27003d88c866f837a285cae57807425e9
이대근 <eorms96@naver.com>
2023-09-12 17:41:56 +0900

    Feat: 견적시스템을 위한 Entity 틀 생성

---
+++ Backend/src/main/java/com/ssafy/pcgg/domain/recommend/entity/QuoteCandidateEntity.java
@@ -0,0 +1,10 @@
+package com.ssafy.pcgg.domain.recommend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quote_candidate")
+public class QuoteCandidateEntity {
+
+}
