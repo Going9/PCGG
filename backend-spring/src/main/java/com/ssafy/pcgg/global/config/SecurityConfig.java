42f3f30d180999f76c4fbd49eb184aba579c9c62
김소이 <thdl9893@ajou.ac.kr>
2023-09-15 13:48:43 +0900

    Feat: 공유마당 관련 원활한 기능 개발을 위해 config 파일에 shares관련 경로 추가

--- backend-spring/src/main/java/com/ssafy/pcgg/global/config/SecurityConfig.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/global/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/api-docs/**").permitAll()
                         .requestMatchers("api/v1/test/**").permitAll()
                         .requestMatchers("api/v1/auth/**").permitAll()
+                        .requestMatchers("api/v1/shares/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
                         .anyRequest().authenticated()
                 )
