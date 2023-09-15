41de1fc9b985efa2fad9d64985937413825602b0
차재호 <ischar@naver.com>
2023-09-15 17:56:56 +0900

    경로명 수정

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -12,17 +12,17 @@
           <img
             alt="profileIcon"
             class="profile"
-            src="@/assets/icon/profileIcon.png"
+            src="@/assets/Icon/profileIcon.png"
           />
         </RouterLink>
         <RouterLink to="/">
-          <img alt="bellIcon" class="bell" src="@/assets/icon/bellIcon.png" />
+          <img alt="bellIcon" class="bell" src="@/assets/Icon/bellIcon.png" />
         </RouterLink>
         <RouterLink to="/">
           <img
             alt="hambergerIcon"
             class="hamberger"
-            src="@/assets/icon/hambergerIcon.png"
+            src="@/assets/Icon/hambergerIcon.png"
           />
         </RouterLink>
       </div>
