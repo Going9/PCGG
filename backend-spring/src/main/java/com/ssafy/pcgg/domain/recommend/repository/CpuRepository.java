dd123290b908ba451ba99bcc9fbb444c85b0f851
김소이 <thdl9893@ajou.ac.kr>
2023-09-19 12:51:34 +0900

    Feat: 추가 PC부품 repository

---
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/recommend/repository/CpuRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.pcgg.domain.recommend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ssafy.pcgg.domain.recommend.entity.CpuEntity;
+
+@Repository
+public interface CpuRepository extends JpaRepository<CpuEntity, Integer> {
+}
