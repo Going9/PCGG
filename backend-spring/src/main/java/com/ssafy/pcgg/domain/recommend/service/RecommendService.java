3a08d0e8c0ee1cd15f5f3da9dca7e921ec1c6d03
이대근 <eorms96@naver.com>
2023-09-21 17:39:54 +0900

    Feat: 견적추천-데스크탑 용도별 경우의 수 생성 초안

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/recommend/service/RecommendService.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/recommend/service/RecommendService.java
@@ -1,5 +1,7 @@
 package com.ssafy.pcgg.domain.recommend.service;
 
+import com.ssafy.pcgg.domain.recommend.entity.*;
+import com.ssafy.pcgg.domain.recommend.exception.ClassifyPartAllFailedException;
 import com.ssafy.pcgg.domain.recommend.exception.ClassifyPartException;
 import com.ssafy.pcgg.domain.recommend.exception.QuoteCandidateException;
 import com.ssafy.pcgg.domain.recommend.repository.*;
@@ -8,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,27 +32,33 @@ public class RecommendService {
     private final PowerRepository powerRepository;
 
     public HttpStatus classifyAndCreateCandidate() {
+        //분류
         try{
             classifyPart();
         } catch(ClassifyPartException e){
-//            logger.error("message","부품 분류 중 오류 발생");
+            logger.error("message","부품 분류 중 오류 발생", e);
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
+        //QuoteCandidate 삭제 후 생성
         try{
-//            recommendUtil.deleteAndCreateQuoteCandidate();
+            deleteAndCreateQuoteCandidate();
         } catch(QuoteCandidateException e){
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
         return HttpStatus.OK;
     }
 
-    public void classifyPart() {
+    public void classifyPart() throws ClassifyPartException{
         List<?> partList;
+        int exceptionCount = 0;
+
+        //exception이 발생해도 다른 부품 분류 계속 진행
         try {
             partList = cpuRepository.findAllByClass(null);
             recommendUtil.classifyCpu(partList);
         } catch(ClassifyPartException e){
             logger.error("cpu 분류 중 에러 발생", e);
+            exceptionCount++;
         }
 
         try{
@@ -56,18 +66,42 @@ public class RecommendService {
             recommendUtil.classifyRam(partList);
         } catch(ClassifyPartException e){
             logger.error("ram 분류 중 에러 발생", e);
+            exceptionCount++;
         }
         try{
             partList = gpuRepository.findAllByClass(null);
             recommendUtil.classifyGpu(partList);
         } catch(ClassifyPartException e){
             logger.error("gpu 분류 중 에러 발생", e);
+            exceptionCount++;
         }
         try{
             partList = powerRepository.findAllByClass(null);
             recommendUtil.classifyPower(partList);
         } catch(ClassifyPartException e){
             logger.error("cpu 분류 중 에러 발생", e);
+            exceptionCount++;
         }
+
+        if(exceptionCount==4) throw new ClassifyPartAllFailedException();
+    }
+
+    @Transactional
+    public HttpStatus deleteAndCreateQuoteCandidate() {
+        //생성 전 기존 리스트 제거
+        //deleteCandidate
+        quoteCandidateRepository.deleteAll();
+
+        //견적용도별로 새로운 리스트 산출
+        //createCandidate
+        List<UsageNsEntity> usageList = usageNsRepository.findAll();
+        for(UsageNsEntity usage : usageList){
+            //부품별 미분류된 리스트 추출
+            List<List<?>> partList = recommendUtil.makePartList(usage);
+
+            //경우의 수 만들고 저장
+            recommendUtil.generateScenario(partList);
+        }
+        return HttpStatus.CREATED;
     }
 }
