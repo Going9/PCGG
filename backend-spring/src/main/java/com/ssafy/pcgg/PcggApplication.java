ececbd3d0e1d9cf2226ff1af396535f9cbb79b39
Jaeho Cha <isitchar@gmail.com>
2023-09-21 10:20:13 +0900

    Feat: 시간 관련 엔티티 및 클래스 추가 - -

--- backend-spring/src/main/java/com/ssafy/pcgg/PcggApplication.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/PcggApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.pcgg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PcggApplication {
 
 	public static void main(String[] args) {
