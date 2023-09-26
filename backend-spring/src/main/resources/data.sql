6f48f1dfaff0f6094feb633acca9ae816b75649a
deaf52 <eorms96@naver.com>
2023-09-27 00:08:10 +0900

    Fix: 오타제거

--- backend-spring/src/main/resources/data.sql
+++ backend-spring/src/main/resources/data.sql
@@ -11,7 +11,7 @@ INSERT INTO peripheral_keyboard (name, lprice, hprice, brand, image_source, link
 INSERT INTO peripheral_keyboard (name, lprice, hprice, brand, image_source, link, extinct) VALUES ('키보드2', 10000, null, 'aa', 'image path url2', 'link2', 0);
 
 INSERT INTO part_type_ns values('cpu'),('gpu'),('ram'),('power'),('mainboard'),('cooler'),('chassis'),('ssd'),('laptop');
-INSERT INTO usage_ns values('가성비사무'),('고성능사무'),('캐주얼게임'),('중사양게임'),('고사양게임'),('일반영상편집'),('전문영상편집'),('3D디자인'),('일반방송'),('캐주얼게임방송'),('고성능게임방송'),('고사양개발')
+INSERT INTO usage_ns values('가성비사무'),('고성능사무'),('캐주얼게임'),('중사양게임'),('고사양게임'),('일반영상편집'),('전문영상편집'),('3D디자인'),('일반방송'),('캐주얼게임방송'),('고성능게임방송'),('고사양개발');
 
 INSERT INTO part_cpu (name, price, image_source, extinct, socket_info, ddr4, ddr5, integrated_graphics, cooler_included) VALUES ('cpu', 100, 'IS', true, '소켓정보', true, true, true, true);
 INSERT INTO part_mainboard (name, price, image_source, extinct, grade, memory_spec, size, pcie3, pcie4, pcie5, m2_count) VALUES ('mainboard', 200, 'IS', false, '점수', '스펙', 30, true, true, true, 1);
@@ -22,4 +22,4 @@ INSERT INTO part_chassis (name, price, image_source, extinct, extended_atx, stan
 INSERT INTO part_power (name, price, image_source, extinct, size, grade, output, depth, free_warranty_period) VALUES ('power', 100, 'IS', true, 1, 'grade', 101, 3.3, 5);
 INSERT INTO part_cooler (name, price, image_source, extinct, form, type, free_warranty_period, height, fan_count, max_fan_noise) VALUES ('cooler', 100, 'IS', true, '소켓정보', '타입', 3, 3.3, 5, 5.1);
 
-INSERT INTO peripheral_type_ns(naame) VALUES ('keyboard'), ('monitor'), ('printer'), ('mouse'), ('etc');
+INSERT INTO peripheral_type_ns(name) VALUES ('keyboard'), ('monitor'), ('printer'), ('mouse'), ('etc');
