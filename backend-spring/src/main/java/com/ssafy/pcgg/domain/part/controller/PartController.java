7ac022cacb6ee9b99e98a181d91b4bc939f49da1
김소이 <thdl9893@ajou.ac.kr>
2023-09-27 15:50:35 +0900

    Comment: 수정 불필요한 주석 삭제

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/part/controller/PartController.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/part/controller/PartController.java
@@ -6,14 +6,11 @@ import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.pcgg.domain.auth.CurrentUserId;
 import com.ssafy.pcgg.domain.part.service.PartService;
-import com.ssafy.pcgg.domain.share.dto.ShareDetailDto;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
