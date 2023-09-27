5d3e6f00ca5387421c1efaa1f662311175875e41
김소이 <thdl9893@ajou.ac.kr>
2023-09-27 11:08:45 +0900

    Feat: 추가 swagger 예시값

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/share/dto/ShareAddRequestDto.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/share/dto/ShareAddRequestDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.pcgg.domain.share.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -8,9 +9,16 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShareAddRequestDto {
+
+	@Schema(description = "제목", example = "제목")
 	private String title;
+
+	@Schema(description = "내용", example = "내용입니다.")
 	private String content;
-	private String imageSource;
+
+	@Schema(description = "한줄설명", example = "한줄설명입니다.")
 	private String summary;
+
 	private ShareAddQuoteRequestDto shareAddQuoteRequestDto;
+
 }
