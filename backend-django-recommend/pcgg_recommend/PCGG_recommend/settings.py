9df74594a63132911dc49db5d8082ce045d4f9a8
Going9 <ickdor123@gmail.com>
2023-09-27 10:50:42 +0900

    Init: connect to server DB

--- backend-django-recommend/pcgg_recommend/PCGG_recommend/settings.py
+++ backend-django-recommend/pcgg_recommend/PCGG_recommend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'PCGG_recommend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pcgg_db',
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': '3306'
     }
 }
 
