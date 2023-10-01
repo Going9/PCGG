5692f47137d449672a4bcc5c5ceaf1da75fea8ea
김소이 <thdl9893@ajou.ac.kr>
2023-10-01 17:40:54 +0900

    Feat: 추가 공유마당 견적 저장 함수

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/auth/dto/AuthLoginRequest.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/auth/dto/AuthLoginRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.pcgg.domain.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -7,8 +8,10 @@ import lombok.Data;
 public class AuthLoginRequest {
 
     @NotNull
+    @Schema(description = "아이디(이메일)" , example = "ssafy1")
     private String email;
 
     @NotNull
+    @Schema(description = "비밀번호" , example = "ssafy")
     private String password;
 }
