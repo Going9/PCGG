ef58d3873cb9a917e8b4d924d5fb563c36172b5a
김소이 <thdl9893@ajou.ac.kr>
2023-10-02 01:59:11 +0900

    Style: 삭제 스웨거 상세정보 설정

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/auth/dto/AuthLoginRequest.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/auth/dto/AuthLoginRequest.java
@@ -1,6 +1,5 @@
 package com.ssafy.pcgg.domain.auth.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -8,10 +7,8 @@ import lombok.Data;
 public class AuthLoginRequest {
 
     @NotNull
-    @Schema(description = "아이디(이메일)" , example = "ssafy1")
     private String email;
 
     @NotNull
-    @Schema(description = "비밀번호" , example = "ssafy")
     private String password;
 }
