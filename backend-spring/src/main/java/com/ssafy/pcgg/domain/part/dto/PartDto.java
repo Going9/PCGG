d7479544e600d8d43f27635a7e737f8d2b4e0313
deaf52 <eorms96@naver.com>
2023-10-06 00:06:50 +0900

    Fix: 부품추천 수정완료

--- backend-spring/src/main/java/com/ssafy/pcgg/domain/part/dto/PartDto.java
+++ backend-spring/src/main/java/com/ssafy/pcgg/domain/part/dto/PartDto.java
@@ -1,9 +1,18 @@
 package com.ssafy.pcgg.domain.part.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class PartDto {
     //부품들의 공통속성을 가지는 클래스
     String name;
     Integer price;
-    String image_source;
-    boolean extinct;
+    String imageSource;
+    Boolean extinct;
 }
