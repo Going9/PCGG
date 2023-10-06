b5c39fb20d4cab64490c9a8e86e3dbb4ec495d0d
JangJoongWon <wnddnjs843@naver.com>
2023-10-06 09:15:52 +0900

    Fix: FE

--- frontend/src/components/Common/ModalComponent/ItemComponent.vue
+++ frontend/src/components/Common/ModalComponent/ItemComponent.vue
@@ -1,37 +1,37 @@
 <script setup>
-  import {defineProps} from 'vue'
-  const { item } = defineProps(['item']);
+import { defineProps } from "vue";
+const { item } = defineProps(["item"]);
 </script>
 
 <template>
   <div class="container basic">
-    <span>제품명 : {{item.name}}</span>
+    <span> &nbsp; {{ item.name }}</span>
   </div>
 </template>
 
 <style scoped>
 .container {
-  border : 0;
+  border: 0;
   width: 100rem;
   height: 3.4375rem;
   flex-shrink: 0;
   text-align: left;
   display: flex;
-  align-items : center;
-  justify-content : start;
+  align-items: center;
+  justify-content: start;
 }
 
 .basic {
   border-radius: var(--Number, 0rem);
-  border: 1px solid rgba(0, 0, 0, 0.30);
-  background: rgba(217, 217, 217, 0.00);
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background: rgba(217, 217, 217, 0);
   color: black;
 }
 
 .selected {
   border-radius: var(--Number, 0rem);
-  border: 1px solid rgba(0, 0, 0, 0.30);
-  background: #4599FC;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background: #4599fc;
   color: white;
 }
 </style>
